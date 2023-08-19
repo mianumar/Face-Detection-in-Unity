9f331897b411f80563d6dc28660080cdbc9b8cf9
Muhammad Umar <umer.ramzan375@gmail.com>
2023-08-19 10:43:14 +0500

    Update UIManager.cs

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
@@ -7,12 +8,14 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Started");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Update");
+
     }
 
 }
