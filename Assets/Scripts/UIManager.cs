42847549036954455c90d4fdf01e4e0a34f994ff
Muhammad Umar <umer.ramzan375@gmail.com>
2023-12-21 18:14:10 +0500

    smallFix
    smallFix

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,7 +8,7 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Started");
+        //Debug.Log("Started");
     }
 
     // Update is called once per frame
